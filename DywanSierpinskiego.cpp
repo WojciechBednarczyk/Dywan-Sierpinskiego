2dc078e1369dfc312498e0f12d637e25976e0ed7
Wojciech Bednarczyk <wojciechbednarczyk25@gmail.com>
2021-10-16 18:58:55 +0200

    Skonczony Dywan Sierpinskiego

--- DywanSierpinskiego.cpp
+++ DywanSierpinskiego.cpp
@@ -22,31 +22,11 @@ point2 prawy_dolny;
 point2 lewy_gorny;
 point2 prawy_gorny;
 float bok_kwadratu=243.0;
-int stopien = 3;
-int stopien_pom=stopien;
-
-
-
-
-void podziel(float bok_kwadratu)
-{
-
-    stopien_pom--;
-    if (stopien_pom != 0)
-    {
-        cout << bok_kwadratu << endl;
-        podziel(bok_kwadratu);
-        stopien_pom = stopien;
-    }
-
-  
-}
+int stopien = 7;
+int stopien_pom=0;
 
 void losuj_kolory()
 {
-    /*float red = (rand() % 101)/100.0;
-    float green = (rand() % 101) / 100.0;
-    float blue = (rand() % 101) / 100.0;*/
     glColor3f(float(rand() % 100) / 100.0, float(rand() % 100) / 100.0, float(rand() % 100) / 100.0);
 }
 
@@ -64,25 +44,194 @@ void rysuj_kwadrat(point2 wierzch1, point2 wierzch2, point2 wierzch3, point2 wie
     glEnd();
 }
 
-void kolejny_kwadrat()
+void kolejny_kwadrat(float bok,point2 lewy_dolny, point2 lewy_gorny, point2 prawy_gorny, point2 prawy_dolny)
 {
-    lewy_gorny[0] = prawy_gorny[0];
-    lewy_dolny[0] = prawy_dolny[0];
-    prawy_gorny[0] = lewy_gorny[0] + bok_kwadratu / 3.0;
-    prawy_dolny[0] = prawy_gorny[0];
-    rysuj_kwadrat(lewy_dolny, lewy_gorny, prawy_gorny, prawy_dolny);
+    point2 wierzch1;
+    point2 wierzch2;
+    point2 wierzch3;
+    point2 wierzch4;
+
+    wierzch1[0] = lewy_dolny[0];
+    wierzch1[1] = lewy_dolny[1];
+
+    wierzch2[0] = lewy_gorny[0];
+    wierzch2[1] = lewy_gorny[1];
+
+    wierzch3[0] = prawy_gorny[0];
+    wierzch3[1] = prawy_gorny[1];
+
+    wierzch4[0] = prawy_dolny[0];
+    wierzch4[1] = prawy_dolny[1];
+
+    wierzch2[0] = wierzch3[0];
+    wierzch1[0] = wierzch4[0];
+    wierzch3[0] = wierzch2[0] + bok;
+    wierzch4[0] = wierzch3[0];
+    rysuj_kwadrat(wierzch1, wierzch2, wierzch3, wierzch4);
 }
-void kolejna_linia()
+void kolejna_linia(float bok, point2 lewy_dolny, point2 lewy_gorny, point2 prawy_gorny, point2 prawy_dolny)
 {
-    lewy_gorny[0] -= 2 * (bok_kwadratu / 3.0);
-    lewy_gorny[1] = lewy_dolny[1];
-    lewy_dolny[0] = lewy_gorny[0];
-    lewy_dolny[1] = lewy_gorny[1] - bok_kwadratu / 3.0;
-    prawy_gorny[0] = lewy_gorny[0] + bok_kwadratu / 3.0;
-    prawy_gorny[1] = lewy_gorny[1];
-    prawy_dolny[0] = prawy_gorny[0];
-    prawy_dolny[1] = lewy_dolny[1];
-    rysuj_kwadrat(lewy_dolny, lewy_gorny, prawy_gorny, prawy_dolny);
+    point2 wierzch1;
+    point2 wierzch2;
+    point2 wierzch3;
+    point2 wierzch4;
+
+    wierzch1[0] = lewy_dolny[0];
+    wierzch1[1] = lewy_dolny[1];
+
+    wierzch2[0] = lewy_gorny[0];
+    wierzch2[1] = lewy_gorny[1];
+
+    wierzch3[0] = prawy_gorny[0];
+    wierzch3[1] = prawy_gorny[1];
+
+    wierzch4[0] = prawy_dolny[0];
+    wierzch4[1] = prawy_dolny[1];
+
+    wierzch2[0] -= 2 * bok;
+    wierzch2[1] = wierzch1[1];
+    wierzch1[0] = wierzch2[0];
+    wierzch1[1] = wierzch2[1] - bok;
+    wierzch3[0] = wierzch2[0] + bok;
+    wierzch3[1] = wierzch2[1];
+    wierzch4[0] = wierzch3[0];
+    wierzch4[1] = wierzch1[1];
+    rysuj_kwadrat(wierzch1, wierzch2, wierzch3, wierzch4);
+}
+void podziel(float bok_kwadratu, point2 lewy_dolny, point2 lewy_gorny, point2 prawy_gorny, point2 prawy_dolny,int stopien_pom)
+{
+    point2 wierzch1;
+    point2 wierzch2;
+    point2 wierzch3;
+    point2 wierzch4;
+    float bok = bok_kwadratu;
+    wierzch1[0] = lewy_dolny[0];
+    wierzch1[1] = lewy_dolny[1];
+
+    wierzch2[0] = lewy_gorny[0];
+    wierzch2[1] = lewy_gorny[1];
+
+    wierzch3[0] = prawy_gorny[0];
+    wierzch3[1] = prawy_gorny[1];
+
+    wierzch4[0] = prawy_dolny[0];
+    wierzch4[1] = prawy_dolny[1];
+    if (stopien_pom != stopien)
+    {
+        //1 kwadrat
+        //lewy_gorny bez zmian
+        bok /= 3.0;
+
+        wierzch1[1] = wierzch2[1] - bok;
+        wierzch3[0] = wierzch2[0] + bok;
+        wierzch4[0] = wierzch2[0] + bok;
+        wierzch4[1] = wierzch1[1];
+        rysuj_kwadrat(wierzch1, wierzch2, wierzch3, wierzch4);
+        stopien_pom++;
+        podziel(bok, wierzch1, wierzch2, wierzch3, wierzch4, stopien_pom);
+    }
+
+
+
+
+    //2 kwadrat
+    kolejny_kwadrat(bok, wierzch1, wierzch2, wierzch3, wierzch4);
+    wierzch2[0] = wierzch3[0];
+    wierzch1[0] = wierzch4[0];
+    wierzch3[0] = wierzch2[0] + bok;
+    wierzch4[0] = wierzch3[0];
+    if (stopien_pom != stopien)
+    {
+        podziel(bok, wierzch1, wierzch2, wierzch3, wierzch4, stopien_pom);
+    }
+    //3 kwadrat
+    kolejny_kwadrat(bok, wierzch1, wierzch2, wierzch3, wierzch4);
+    wierzch2[0] = wierzch3[0];
+    wierzch1[0] = wierzch4[0];
+    wierzch3[0] = wierzch2[0] + bok;
+    wierzch4[0] = wierzch3[0];
+    if (stopien_pom != stopien)
+    {
+        podziel(bok, wierzch1, wierzch2, wierzch3, wierzch4, stopien_pom);
+    }
+
+    //4 kwadrat
+    kolejna_linia(bok, wierzch1, wierzch2, wierzch3, wierzch4);
+    wierzch2[0] -= 2 * bok;
+    wierzch2[1] = wierzch1[1];
+    wierzch1[0] = wierzch2[0];
+    wierzch1[1] = wierzch2[1] - bok;
+    wierzch3[0] = wierzch2[0] + bok;
+    wierzch3[1] = wierzch2[1];
+    wierzch4[0] = wierzch3[0];
+    wierzch4[1] = wierzch1[1];
+    if (stopien_pom != stopien)
+    {
+        podziel(bok, wierzch1, wierzch2, wierzch3, wierzch4, stopien_pom);
+    }
+
+    //5 kwadrat pusty!
+
+    wierzch2[0] = wierzch3[0];
+    wierzch1[0] = wierzch4[0];
+    wierzch3[0] = wierzch2[0] + bok;
+    wierzch4[0] = wierzch3[0];
+    glBegin(GL_POLYGON);
+    glColor3f(0.5f, 0.5f, 0.5f);
+    glVertex2fv(wierzch1);
+    glVertex2fv(wierzch2);
+    glVertex2fv(wierzch3);
+    glVertex2fv(wierzch4);
+    glEnd();
+
+    //6 kwadrat
+    kolejny_kwadrat(bok, wierzch1, wierzch2, wierzch3, wierzch4);
+    wierzch2[0] = wierzch3[0];
+    wierzch1[0] = wierzch4[0];
+    wierzch3[0] = wierzch2[0] + bok;
+    wierzch4[0] = wierzch3[0];
+    if (stopien_pom != stopien)
+    {
+        podziel(bok, wierzch1, wierzch2, wierzch3, wierzch4, stopien_pom);
+    }
+
+    //7 kwadrat
+    kolejna_linia(bok, wierzch1, wierzch2, wierzch3, wierzch4);
+    wierzch2[0] -= 2 * bok;
+    wierzch2[1] = wierzch1[1];
+    wierzch1[0] = wierzch2[0];
+    wierzch1[1] = wierzch2[1] - bok;
+    wierzch3[0] = wierzch2[0] + bok;
+    wierzch3[1] = wierzch2[1];
+    wierzch4[0] = wierzch3[0];
+    wierzch4[1] = wierzch1[1];
+    if (stopien_pom != stopien)
+    {
+        podziel(bok, wierzch1, wierzch2, wierzch3, wierzch4, stopien_pom);
+    }
+
+    ////8 kwadrat
+    kolejny_kwadrat(bok, wierzch1, wierzch2, wierzch3, wierzch4);
+    wierzch2[0] = wierzch3[0];
+    wierzch1[0] = wierzch4[0];
+    wierzch3[0] = wierzch2[0] + bok;
+    wierzch4[0] = wierzch3[0];
+    if (stopien_pom != stopien)
+    {
+        podziel(bok, wierzch1, wierzch2, wierzch3, wierzch4, stopien_pom);
+    }
+
+    ////9 kwadrat
+    kolejny_kwadrat(bok, wierzch1, wierzch2, wierzch3, wierzch4);
+    wierzch2[0] = wierzch3[0];
+    wierzch1[0] = wierzch4[0];
+    wierzch3[0] = wierzch2[0] + bok;
+    wierzch4[0] = wierzch3[0];
+    if (stopien_pom != stopien)
+    {
+        podziel(bok, wierzch1, wierzch2, wierzch3, wierzch4, stopien_pom);
+    }
+
 }
 void dywan_sierpinskiego()
 {
@@ -97,62 +246,14 @@ void dywan_sierpinskiego()
     prawy_gorny[0] = bok_kwadratu / 2.0;
     prawy_gorny[1] = bok_kwadratu / 2.0;
 
-    
-
     glClear(GL_COLOR_BUFFER_BIT);
     // Czyszczenie okna aktualnym kolorem czyszczącym
 
+    //rysuje glowny kwadrat
     rysuj_kwadrat(lewy_dolny, lewy_gorny, prawy_gorny, prawy_dolny);
 
-    /*
+    podziel(bok_kwadratu, lewy_dolny, lewy_gorny, prawy_gorny, prawy_dolny,stopien_pom);
     
-    Ponizej trzeba zaczac rekurencje
-    
-    */
-
-    //1 kwadrat
-    //lewy_gorny bez zmian
-    lewy_dolny[1] = lewy_gorny[1] - bok_kwadratu / 3.0;
-    prawy_gorny[0] = lewy_gorny[0] + bok_kwadratu / 3.0;
-    prawy_dolny[0] = lewy_gorny[0] + bok_kwadratu / 3.0;
-    prawy_dolny[1] = lewy_dolny[1];
-    rysuj_kwadrat(lewy_dolny,lewy_gorny,prawy_gorny,prawy_dolny);
- 
-    //2 kwadrat
-    kolejny_kwadrat();
-
-    //3 kwadrat
-    kolejny_kwadrat();
-
-    //4 kwadrat
-    kolejna_linia();
-
-    //5 kwadrat pusty!
-
-    lewy_gorny[0] = prawy_gorny[0];
-    lewy_dolny[0] = prawy_dolny[0];
-    prawy_gorny[0] = lewy_gorny[0] + bok_kwadratu / 3.0;
-    prawy_dolny[0] = prawy_gorny[0];
-
-    glBegin(GL_POLYGON);
-    glColor3f(0.5f, 0.5f, 0.5f);
-    glVertex2fv(lewy_dolny);
-    glVertex2fv(lewy_gorny);
-    glVertex2fv(prawy_gorny);
-    glVertex2fv(prawy_dolny);
-    glEnd();
-
-    //6 kwadrat
-    kolejny_kwadrat();
-
-    //7 kwadrat
-    kolejna_linia();
-
-    //8 kwadrat
-    kolejny_kwadrat();
-
-    //9 kwadrat
-    kolejny_kwadrat();
 }
 
 // Funkcaja określająca, co ma być rysowane
